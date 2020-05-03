92f24933a758d4c2f15f3d3226c7be0cee467317
Thanh Bui <congthanh184@gmail.com>
2020-05-03 22:08:05 +0700

    Add add_markers.sh to test markers

---
+++ scripts/add_markers.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+gnome-terminal -- roslaunch my_robot simple.launch &
+sleep 5
+
+gnome-terminal -- roslaunch my_robot amcl.launch &
+sleep 5
+
+gnome-terminal -- rosrun add_markers add_markers &
+
