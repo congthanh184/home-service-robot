7cd58c11c24a94118a30f94720990d865f5d739f
Thanh Bui <congthanh184@gmail.com>
2020-05-04 21:46:36 +0700

    Add home_service script

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -3,9 +3,13 @@
 gnome-terminal -- roslaunch my_robot simple.launch &
 sleep 5
 
-gnome-terminal -- rosrun add_markers add_markers _env:=test &
+gnome-terminal -- rosrun add_markers add_markers &
 sleep 1
 
 gnome-terminal -- roslaunch my_robot amcl.launch &
+sleep 5
+
+gnome-terminal -- rosrun pick_objects simple_navigation_goals &
+
 
 
